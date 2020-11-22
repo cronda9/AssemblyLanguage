722c7eea3c81dd6c20e148a1236025ec598c33ed
Christian Ronda <cronda@princeton.edu>
2020-11-22 16:10:50 -0500

    endCarry

--- bigintaddopt.s
+++ bigintaddopt.s
@@ -213,8 +213,8 @@ endOverflow2:
 
     // oSum->aulDigits[lIndex] = ulSum;
     add x0, OSUM, LDIGITS
-    ldr x0, [x0, LINDEX, lsl 3]
-    mov ULSUM, x0
+    add x0, [x0, LINDEX, lsl 3]
+    mov x0, ULSUM // CHANGED
 
     // lIndex++;
     add LINDEX, LINDEX, 1
@@ -252,7 +252,7 @@ endMaxDigits:
 
     // oSum->aulDigits[lSumLength] = 1;
     add x0, OSUM, LDIGITS
-    ldr x0, [x0, LINDEX, lsl 3]
+    add x0, [x0, LINDEX, lsl 3]  // CHANGED
     mov x0, 1
 
     // lSumLength++;
@@ -263,8 +263,7 @@ endCarry:
     // Set the length of the sum.
     // oSum->lLength = lSumLength;
     add x0, OSUM, LLENGTH
-    ldr x0, [x0]
-    mov x0, LSUMLENGTH
+    mov x0, LSUMLENGTH // CHANGED
 
     // Epilogue and return TRUE;
     mov x0, TRUE
